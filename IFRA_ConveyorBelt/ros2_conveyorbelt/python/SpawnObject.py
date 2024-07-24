8f9306f1371212ec3810b138bdb534b52131b096
rkroshan <rkroshan.1999@gmail.com>
2024-07-24 21:59:45 +0530

    spawn random color box at 30sec interval
    Signed-off-by: rkroshan <rkroshan.1999@gmail.com>

--- IFRA_ConveyorBelt/ros2_conveyorbelt/python/SpawnObject.py
+++ IFRA_ConveyorBelt/ros2_conveyorbelt/python/SpawnObject.py
@@ -33,35 +33,27 @@
 # IFRA-Cranfield (2023) ROS 2 Sim-to-Real Robot Control. URL: https://github.com/IFRA-Cranfield/ros2_SimRealRobotControl -> ros2srrc_execution ROS2 Package.
 
 # IMPORT LIBRARIES:
-import argparse
 import os
-import xacro
 from ament_index_python.packages import get_package_share_directory
 from gazebo_msgs.srv import SpawnEntity
 import rclpy
-
+import time
+import random
+import uuid
 # Reference to SPAWN OBJECT (.urdf or .xacro file) from the terminal shell:
 # REFERENCE: ros2 run ros2_conveyorbelt SpawnObject.py --package "{}" --urdf "{}.urdf" --name "{}" --x {} --y {} --z {}
 # EXAMPLE: BOX -> ros2 run ros2_conveyorbelt SpawnObject.py --package "conveyorbelt_gazebo" --urdf "box.urdf" --name "box" --x 0.0 --y -0.5 --z 0.76
 
+sdf_package_name = 'conveyorbelt_gazebo'
+sdf_relative_location = 'models/aruco_marker'
+boxes_type = ['model_red.sdf', 'model_blue.sdf', 'model_green.sdf']
+
+Duration_btw_bxs_spawn_sec = 30
 
 def main():
-    # Get input arguments from user
-    parser = argparse.ArgumentParser(description='Spawn object into our Gazebo world.')
-    parser.add_argument('--package', type=str, default='', help='Package where URDF/XACRO file is located.')
-    parser.add_argument('--urdf', type=str, default='', help='URDF of the object to spawn.')
-    parser.add_argument('--name', type=str, default='OBJECT', help='Name of the object to spawn.')
-    parser.add_argument('--namespace', type=str, default='ros2Grasp', help='ROS namespace to apply to the tf and plugins.')
-    parser.add_argument('--ns', type=bool, default=True, help='Whether to enable namespacing')
-    parser.add_argument('--x', type=float, default=0.0, help='the x component of the initial position [meters].')
-    parser.add_argument('--y', type=float, default=0.0, help='the y component of the initial position [meters].')
-    parser.add_argument('--z', type=float, default=0.0, help='the z component of the initial position [meters].')
-    
-    args, unknown = parser.parse_known_args()
-
     # Start node:
     rclpy.init()
-    node = rclpy.create_node('entity_spawner')
+    node = rclpy.create_node('entity_spawner_boxes_model')
 
     node.get_logger().info(
         'Creating Service client to connect to `/spawn_entity`')
@@ -74,28 +66,34 @@ def main():
 
     # Set data for request:
     request = SpawnEntity.Request()
-    request.name = args.name
 
-    urdf_file_path = os.path.join(get_package_share_directory(args.package), 'urdf', args.urdf) # It is assumed that the .urdf/.xacro file is located in /urdf folder!
-    xacro_file = xacro.process_file(urdf_file_path)
-    request.xml = xacro_file.toxml() 
+    try:
+        while True:
+            _rand_box_num = random.randint(0,2)
+            request.name = str(uuid.uuid4())
+            sdf_file_path = os.path.join(get_package_share_directory(sdf_package_name), sdf_relative_location, boxes_type[_rand_box_num]) 
+            content = ""
+            with open(sdf_file_path, 'r') as content_file:
+                content = content_file.read()
+            request.xml = content
 
-    request.initial_pose.position.x = float(args.x)
-    request.initial_pose.position.y = float(args.y)
-    request.initial_pose.position.z = float(args.z)
+            request.initial_pose.position.x = 0.0
+            request.initial_pose.position.y = -1.8
+            request.initial_pose.position.z = 0.76
 
-    if args.namespace is True:
-        node.get_logger().info('spawning `{}` on namespace `{}` at {}, {}, {}'.format(
-            args.name, args.namespace, args.x, args.y, args.z))
+            node.get_logger().info('Spawning OBJECT using service: `/spawn_entity`')
+            future = client.call_async(request)
+            rclpy.spin_until_future_complete(node, future)
+            if future.result() is not None:
+                print('response: %r' % future.result())
+            else:
+                raise RuntimeError(
+                    'exception while calling service: %r' % future.exception())
+            #sleep
+            time.sleep(Duration_btw_bxs_spawn_sec)
+    except Exception as e:
+        print(e)
 
-        request.namespace = args.namespace
-        print(args.namespace)
-
-    else:
-        node.get_logger().info('spawning `{}` at {}, {}, {}'.format(
-            args.name, args.x, args.y, args.z))
-
-    node.get_logger().info('Spawning OBJECT using service: `/spawn_entity`')
     future = client.call_async(request)
     rclpy.spin_until_future_complete(node, future)
     if future.result() is not None:
