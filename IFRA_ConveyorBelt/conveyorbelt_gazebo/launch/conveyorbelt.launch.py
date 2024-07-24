66c3ba75badb32aa3b67cfa8be9ce994935ebac4
rkroshan <rkroshan.1999@gmail.com>
2024-07-24 23:59:15 +0530

    enhance: ui of env , removed visual for belt, changes color of belt to white, deleted unwanted files
    Signed-off-by: rkroshan <rkroshan.1999@gmail.com>

--- IFRA_ConveyorBelt/conveyorbelt_gazebo/launch/conveyorbelt.launch.py
+++ IFRA_ConveyorBelt/conveyorbelt_gazebo/launch/conveyorbelt.launch.py
@@ -37,34 +37,10 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
-from launch.actions import ExecuteProcess, IncludeLaunchDescription, RegisterEventHandler
-from launch.event_handlers import OnProcessExit
+from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import PathJoinSubstitution
-import xacro
-import yaml
 
-# LOAD FILE:
-def load_file(package_name, file_path):
-    package_path = get_package_share_directory(package_name)
-    absolute_file_path = os.path.join(package_path, file_path)
-    try:
-        with open(absolute_file_path, 'r') as file:
-            return file.read()
-    except EnvironmentError:
-        # parent of IOError, OSError *and* WindowsError where available.
-        return None
-# LOAD YAML:
-def load_yaml(package_name, file_path):
-    package_path = get_package_share_directory(package_name)
-    absolute_file_path = os.path.join(package_path, file_path)
-    try:
-        with open(absolute_file_path, 'r') as file:
-            return yaml.safe_load(file)
-    except EnvironmentError:
-        # parent of IOError, OSError *and* WindowsError where available.
-        return None
-    
 # ========== **GENERATE LAUNCH DESCRIPTION** ========== #
 def generate_launch_description():
     
